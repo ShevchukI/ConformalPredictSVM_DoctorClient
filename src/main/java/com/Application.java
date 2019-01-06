f7a9ac12e3442e2145d2002302da6eea118f25e8
ShevchukI <ihShevchuk@gmail.com>
2019-01-06 21:44:06 +0200

    First commit

---
+++ src/main/java/com/Application.java
@@ -0,0 +1,29 @@
+package com;
+
+import com.controllers.LoginMenuController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loginMenuLoader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/loginMenu.fxml"));
+        Pane loginMenuPane = (Pane)loginMenuLoader.load();
+        Scene loginMenuScene = new Scene(loginMenuPane);
+        stage.setScene(loginMenuScene);
+        stage.setResizable(false);
+        stage.setTitle("DocClient");
+        LoginMenuController loginMenuController = (LoginMenuController)loginMenuLoader.getController();
+        loginMenuController.setStage(stage);
+        stage.show();
+
+    }
+}
